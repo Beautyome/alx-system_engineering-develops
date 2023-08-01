b0e11e1f1356751b3f708ed5d6a2c98fdfb624cc
Beautyome <beautyupeleomo@gmail.com>
2023-08-01 14:49:25 -0700

    Regular expression

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,17 @@
-#!/usr/bin/env ruby
-#puts ARGV[0].scan(/School/).join
+heck if the argument is provided
+if ARGV.length != 1
+    puts "Usage: #{$PROGRAM_NAME} <string>"
+      exit 1
+end
+
+# Define the regular expression
+# pattern = /School/
+#
+# # Get the argument from the command line
+# input_string = ARGV[0]
+#
+# # Perform the regular expression match
+# result = input_string.scan(pattern)
+#
+# # Print the result
+# puts result.join
