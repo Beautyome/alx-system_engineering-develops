e19b5f1a9d8f598516b1a72e697a2d541abb88f6
Beautyome <beautyupeleomo@gmail.com>
2023-11-09 15:57:59 -0800

    API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Function to count words in all hot posts of a given Reddit subreddit."""
+import requests
+
+
 def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given words found in hot posts of a given subreddit.
 
@@ -22,7 +26,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
                             allow_redirects=False)
     try:
         results = response.json()
-        if response.status_code == 404 or results is None:  # Add check for None
+        if response.status_code == 404:
             raise Exception
     except Exception:
         print("")
@@ -49,4 +53,3 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         [print("{}: {}".format(k, v)) for k, v in instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
-
